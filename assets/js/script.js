28ba83f4997caa77033e45f7d337307d4389e1f5
joe <hudakjoe@msn.com>
2021-11-27 21:52:14 -0500

    added 1st question in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,27 @@
-function setInput(button) {
-  var buttonVal = button.name;
-  button.style.display = 'none';
-  textbox = document.getElementById('input_' + buttonVal);
-  textbox.value = Text;
+const startButton = document.getElementById('start-btn');
+const questionContainerElement = document.getElementById('question-container');
+
+startButton.addEventListener('click', startGame);
+
+function startGame() {
+  console.log('started');
+  startButton.classList.add('hide');
+  questionContainerElement.classList.remove('hide');
+  setNextQuestion();
 }
+function setNextQuestion() {}
+
+function selectAnswer() {}
+
+const questions = [
+  {
+    question:
+      'string values must be enclosed within ___ when being assigned to variables',
+    answers: [
+      { text: 'commas', correct: false },
+      { text: 'curly brackets', correct: true },
+      { text: 'quotes', correct: false },
+      { text: 'parenthesis', correct: false },
+    ],
+  },
+];
